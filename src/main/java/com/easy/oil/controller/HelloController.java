099f1e0f27c2f967b1b02cef025ddb3a0c7a1624
jaye1944 <jaye1944@gmail.com>
2013-11-22 12:13:09 +0530

    login in logic implemented without security

--- src/main/java/com/easy/oil/controller/HelloController.java
+++ src/main/java/com/easy/oil/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.easy.oil.controller;
 
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +10,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.ui.ModelMap;
 
+import com.easy.oil.data.UserRepository;
+import com.easy.oil.data.User;
+import com.easy.oil.data.BeanConfiguration;
 
 @Controller
 public class HelloController {
-	
-	
+		
+		//db connection propreties
+	  AbstractApplicationContext context = new AnnotationConfigApplicationContext(BeanConfiguration.class);
+	  UserRepository repository = context.getBean(UserRepository.class);
 //logging page login request come to this page
 	   @RequestMapping(value = "/login", method = RequestMethod.GET)
 	   public ModelAndView user() {
@@ -24,14 +31,22 @@ public class HelloController {
 	   @RequestMapping(value = "/admin_user", method = RequestMethod.POST)
 	   public ModelAndView logUser(@ModelAttribute("SpringWeb")Reader reader, 
 	   ModelMap model) {
-	      model.addAttribute("name", reader.getName());
-	      model.addAttribute("password", reader.getPass()); 
+	   /*   model.addAttribute("name", reader.getName());
+	      model.addAttribute("password", reader.getPass()); */
 	      //DB check must goes under here
-	      //if user
-	      //return "news_view";
-	      //if admin
-	     // 
-	      return new ModelAndView("news_post", "command", new News());
+		   Iterable<User> users = repository.findAll();new ModelAndView("login", "command", new Reader());
+		   for (Object user : users) {
+				User cc = (User) user;
+				if (cc.getUsername()==reader.getName() && cc.getPassword()== reader.getPass()){
+					if(cc.getAdmin()==true ){
+						return new ModelAndView("news_post", "command", new News());
+					}else{
+						//return user view
+					}								
+				}				
+			}
+	      //if not in db return user login
+		  return new ModelAndView("login", "command", new Reader());
 	   }
 	   
 //after write pass word and 
