bfbe3d597b42477624836c816c304bc7744b1a97
jaye1944 <jaye1944@gmail.com>
2013-11-24 22:10:42 +0530

    change sql file

--- localhost.sql
+++ localhost.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 24, 2013 at 01:29 AM
+-- Generation Time: Nov 24, 2013 at 10:08 PM
 -- Server version: 5.5.32-0ubuntu0.13.04.1
 -- PHP Version: 5.4.9-4ubuntu2.3
 
@@ -35,23 +35,31 @@ CREATE TABLE IF NOT EXISTS `News` (
   `date` varchar(255) DEFAULT NULL,
   `headline` varchar(255) DEFAULT NULL,
   `user_id` varchar(255) DEFAULT NULL,
+  `timestmp` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `News`
+--
+
+INSERT INTO `News` (`id`, `content`, `cost`, `date`, `headline`, `user_id`, `timestmp`) VALUES
+(1, 'go go not goyt ', '10222222', NULL, 'first newa again ', '1', '3913-11-10 10:10:10');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `User`
+-- Table structure for table `StdUser`
 --
 
-CREATE TABLE IF NOT EXISTS `User` (
+CREATE TABLE IF NOT EXISTS `StdUser` (
   `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(20) NOT NULL,
   `last_name` varchar(20) NOT NULL,
   `e_mail` varchar(30) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `admin` tinyint(1) NOT NULL,
+  `administrator` tinyint(1) NOT NULL,
   `currency` varchar(5) NOT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `username` (`username`),
@@ -61,10 +69,10 @@ CREATE TABLE IF NOT EXISTS `User` (
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
--- Dumping data for table `User`
+-- Dumping data for table `StdUser`
 --
 
-INSERT INTO `User` (`user_id`, `first_name`, `last_name`, `e_mail`, `username`, `password`, `admin`, `currency`) VALUES
+INSERT INTO `StdUser` (`user_id`, `first_name`, `last_name`, `e_mail`, `username`, `password`, `administrator`, `currency`) VALUES
 (1, 'Amal', 'Gunadasa', 'Amal@gmail.com', 'asd', '123', 1, 'USD'),
 (2, 'Blan', 'Kumara', 'Kumara@gmail.com', 'qwe', '345', 1, 'RS'),
 (3, 'kasun', 'kumara', 'kumara@gmail.com', 'qq', '11', 0, 'Rs');
