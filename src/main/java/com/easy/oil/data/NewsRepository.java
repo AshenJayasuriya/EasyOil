0796297a83b4e979170343be2e085458ab999473
ctharindace <ctharinda4114@gmail.com>
2013-11-27 16:59:04 +0530

    Modifications to the news_view page

--- src/main/java/com/easy/oil/data/NewsRepository.java
+++ src/main/java/com/easy/oil/data/NewsRepository.java
@@ -1,9 +1,15 @@
 package com.easy.oil.data;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NewsRepository extends CrudRepository<News, Long>{
+public interface NewsRepository extends CrudRepository<News, Long >{
+	
+	
+	//Getting the latest news
+	//@Query ("SELECT n FROM news WHERE n.timestmp=(SELECT MAX(timestamp) FROM news) LIMIT = 1")
+	//public News findMaxTimestamp();
 
 }
